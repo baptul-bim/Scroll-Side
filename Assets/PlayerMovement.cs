6d44da468b458a61d843329c3a3009b6b30f0ec3
baptul-bim <baptul.bim@gmail.com>
2022-12-08 15:17:33 +0100

    movement fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    float playerSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //horizontal movement
+        if (Input.GetKey("d"))
+        {
+            transform.position = transform.position += transform.right * playerSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.position = transform.position -= transform.right * playerSpeed * Time.deltaTime;
+
+        }
+
     }
 }
